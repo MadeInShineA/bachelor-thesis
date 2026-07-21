3bbafcacea6bbeb7749cca5ddedeaed1951df83c
Olivier <olivier.amacker@netplus.ch>
2026-07-21 11:59:46 +0900

    wip: Start CBI presentation

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
   };
 
   outputs =
-
     {
       self,
       nixpkgs,
@@ -28,6 +27,15 @@
             codebook
           ];
 
+          # Automatically handles the Quarto acronyms extension on shell startup
+          shellHook = ''
+            if [ ! -d "_extensions/rchaput/acronyms" ]; then
+              echo "Installing rchaput/acronyms extension..."
+              quarto add rchaput/acronyms --no-prompt
+            else
+              echo "Acronyms extension is already present."
+            fi
+          '';
         };
       }
     );
